d7f6e4dc097d5d1ce5a95588728ceefdb3acde7d
Clint <clint.m@freshclinics.com>
2024-04-09 13:55:14 +0800

    added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const josefinSans = localFont({
   src: [
@@ -33,6 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={josefinSans.className}>{children}</body>
     </html>
   );
