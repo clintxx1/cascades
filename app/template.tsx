b5c6044874679232c33fcd1db6ef9d77f1772289
Clint <farmingislove@gmail.com>
2024-06-27 21:21:35 +0800

    chore: hide navbar in admin page

--- app/template.tsx
+++ app/template.tsx
@@ -2,11 +2,14 @@
 
 import Navbar from "@/components/navbar";
 import "./globals.css";
+import { usePathname } from "next/navigation";
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const path = usePathname();
+  const showNavBar = path === "/admin";
   return (
     <div className="w-full h-screen overflow-auto relative">
-      <Navbar />
+      {!showNavBar && <Navbar />}
       {children}
     </div>
   );
